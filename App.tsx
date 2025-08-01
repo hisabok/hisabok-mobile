a036d7772357470fb0cb2ae746b7c8d6584e2310
Tanmay Gupta <tanmay631@gmail.com>
2025-08-01 14:59:01 +0530

    project initialized

---
+++ App.tsx
@@ -0,0 +1,15 @@
+// import "./global.css";
+import React from "react";
+import { Text, View } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <View >
+      <Text>
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+};
+
+export default App;
